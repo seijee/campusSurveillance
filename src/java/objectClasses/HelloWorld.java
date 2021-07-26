8b4df7cdc9208bc400771b1ca7cf8ae01b20f471
seijee <seijee@users.noreply.github.com>
2021-07-27 01:08:08 +0530

    Update HelloWorld.java

--- src/java/objectClasses/HelloWorld.java
+++ src/java/objectClasses/HelloWorld.java
@@ -2,7 +2,7 @@ class HelloWorld {
     public static void main( String []args ) {
         System.out.println( "Hello World!" );
         //Adding a Pull Request....
-
+        // Updates 2.0 From Charchit
         //charan  
 
   }
